093bec97c2ff295578b3b305d4a4038c526eb5df
Victor Kuryanov <kvitex@gmail.com>
2020-07-09 18:52:39 +0300

    added index and primary_ip_address

--- netbox2tf.py
+++ netbox2tf.py
@@ -20,23 +20,29 @@ netbox_ssl_verify           = true_false.get(os.environ.get('NETBOX_SSL_VERIFY',
 tf_file_template            = os.environ.get('TF_FILE_TEMPLATE','nb-template.tf.j2')
 nbdev_tf_var_name           = os.environ.get('TF_VAR_NAME','nb_devices')
 
+
 def main():
     session = requests.Session()
     session.verify = netbox_ssl_verify
     nb = pynetbox.api(url=netbox_url, private_key=netbox_private_key, token=netbox_token)
     nb.http_session = session
+    nb_ips = nb.ipam.ip_addresses.all()
     nb_devs = nb.virtualization.virtual_machines.all()
     all_devices= {}
-    device_type='vm'
+    device_type='VM'
     for nb_dev in nb_devs:
         device = nb_dev.serialize()
         device['device_type'] = device_type
         resource_name = re.sub('[^0-9a-zA-Z]+', '_', device['name'])
         device['tags'] = ','.join(device['tags'])
         device['custom_fields'] = json.dumps(device.get('custom_fields',{}))
+        if device['primary_ip'] is not None:
+            for ip in nb_ips:
+                if ip.id == device['primary_ip']:
+                    device['primary_ip_addr'] = str(ip).split('/')[0]
         all_devices[resource_name] = device
     nb_devs = nb.dcim.devices.all()
-    device_type = 'baremetal'
+    device_type = 'Baremetal'
     for nb_dev in nb_devs:
         device = nb_dev.serialize()
         device['device_type'] = device_type
